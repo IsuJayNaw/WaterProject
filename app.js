07f1547bdc829663d1446a857edbf764521662a7
IsuJay <isujayawardana1220@gmail.com>
2020-10-22 19:08:49 +0530

    js page

---
+++ app.js
@@ -0,0 +1,13 @@
+const height = document.getElementById('height');
+const width = document.getElementById('width');
+const btn = document.getElementById('submit');
+
+btn.addEventListener('click', (e)=>{
+
+    const heightVal = height.value;
+    const widthVal = width.value;
+
+    console.log(heightVal);
+    console.log();
+
+});
