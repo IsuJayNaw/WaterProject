ecde101106a5e4211930677c046fce7106ea512c
IsuJay <isujayawardana1220@gmail.com>
2020-10-26 19:22:16 +0530

    Change Css

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const height = document.getElementById('height');
 const weight = document.getElementById('weight');
 const btn = document.getElementById('submit');
+const content = document.getElementById('content');
+const result = document.getElementById('result')
 
 btn.addEventListener('click', (e)=>{
 
@@ -11,6 +13,12 @@ btn.addEventListener('click', (e)=>{
     let pound = 2.20462 * weightVal;
     let ounce = (pound/3) * 2;
     let liter = Math.round(ounce * 0.0295735);
+
+    //content.style.display = "none";
+    const html = `<div class="card text-white bg-primary mb-3 mx-auto" id ="result">
+                    <div class="result"> You should drink ${liter} Liter </div>
+                  </div>`
+    result.innerHTML = html;
 
    
 
